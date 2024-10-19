1b7ca3f93b32379c85004d8e54d83b06577b84a5
Chyriii <etianlloyd21@gmail.com>
2024-10-19 11:07:51 +0800

    2nd Commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,19 +1,17 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Toast
-import androidx.appcompat.widget.Toolbar
+import android.widget.Toast // Import Toast class
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.example.myapplication.databinding.ActivityMainBinding
 import com.google.android.material.navigation.NavigationView
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
@@ -37,23 +35,32 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         binding.bottomnavBar.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.bottom_home -> openFragment(HomeFragment())
-                R.id.bottom_search -> openFragment(SearchFragment())
+                R.id.bottom_search -> openFragment(SettingFragment())
                 R.id.bottom_playlist -> openFragment(PlaylistFragment())
                 R.id.bottom_favorites -> openFragment(FavoritesFragment())
             }
             true
         }
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, SettingFragment())
+            .commit()
         fragmentManager = supportFragmentManager
         openFragment(HomeFragment())
-
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.nav_about -> openFragment(AboutFragment())
-            R.id.nav_home -> openFragment(HomeFragment())
+        when (item.itemId) {
+            R.id.nav_new -> openFragment(NewFragment())
+            R.id.nav_history -> openFragment(HistoryFragment())
             R.id.nav_profile -> openFragment(ProfileFragment())
             R.id.nav_settings -> openFragment(SettingFragment())
+            R.id.nav_logout -> {
+                // Handle logout
+                Toast.makeText(this, "Logged out", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish() // Close MainActivity
+            }
         }
         binding.DrawerLayout.closeDrawer(GravityCompat.START)
         return true
@@ -66,7 +73,8 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
             super.onBackPressed()
         }
     }
-    private fun openFragment(fragment: Fragment){
+
+    private fun openFragment(fragment: Fragment) {
         val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.fragment_container, fragment)
         fragmentTransaction.commit()
